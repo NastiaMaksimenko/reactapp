7dd4dcfe4a0e75912d4eaeab2549b115e38ddb56
wydra76yan <yan@vydra.com>
2018-03-19 11:38:55 +0300

    Test Commit

--- src/index.js
+++ src/index.js
@@ -1,67 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import {render} from 'react-dom'
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {this.props.value}
-      </button>
-    );
-  }
+function Article() {
+  const body = <section>body</section>
+  return(
+    <div classname = "hello" style={{color:'green'}}>
+      <h2>title</h2>
+      {body}
+      <h3>creation date: {(new Date()).toDateString()}</h3>
+    </div>
+  )
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
-  }
-
-  render() {
-    const status = 'Next player: X';
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
+function App(){
+  return(
+    <div>
+      <h1>App Name</h1>
+      <Article/>
+    </div>
+  )
 }
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-// ========================================
-
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
+render(<App/>, document.getElementById('root'))
